71729520d0a37bb4ddff7f738a0bdaab740f0fe6
Henry Torres <henrythhf@gmail.com>
2025-06-11 22:03:13 -0600

    Agregando la base de datos

---
+++ biblioteca-vistas/src/main/java/componentes/ConexionDB.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package componentes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author henry-torres
+ */
+public class ConexionDB {
+
+    private static final String URL = "jdbc:h2:./basedatos/biblioteca"; // crea la base en carpeta "basedatos"
+    private static final String USER = "sa";
+    private static final String PASSWORD = ""; 
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos H2: " + e.getMessage());
+            return null;
+        }
+    }
+}
