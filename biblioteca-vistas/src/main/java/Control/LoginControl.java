759088c295f0c7a160156e9e8f7458b91182421f
Bryan <pmbryan3@gmail.com>
2025-06-13 18:48:33 -0600

    Agrego temporizadores al login y pantalla principal

--- biblioteca-vistas/src/main/java/Control/LoginControl.java
+++ biblioteca-vistas/src/main/java/Control/LoginControl.java
@@ -6,10 +6,12 @@ import javax.swing.*;
 
 public class LoginControl {
     private Login loginVista;
+    private Timer cierreTimer; 
 
     public LoginControl(Login loginVista) {
         this.loginVista = loginVista;
         initEventos();
+        iniciarTimerCierre(); 
     }
 
     private void initEventos() {
@@ -21,13 +23,28 @@ public class LoginControl {
         String password = new String(loginVista.getTxtContraseña().getPassword());
         String rol = (String) loginVista.getCbRol().getSelectedItem();
 
-       if (Aut.autenticar(usuario, password, rol)) { 
+        if (Aut.autenticar(usuario, password, rol)) {
+            
+            if (cierreTimer != null && cierreTimer.isRunning()) {
+                cierreTimer.stop();
+            }
+
             SwingUtilities.invokeLater(() -> {
                 PrincipalControl principalControl = new PrincipalControl(usuario, rol);
             });
             loginVista.dispose();  
         } else {
             JOptionPane.showMessageDialog(loginVista, "Usuario o contraseña incorrectos", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
-}
+
+    private void iniciarTimerCierre() {
+        cierreTimer = new Timer(60000, e -> {
+            JOptionPane.showMessageDialog(loginVista, "Tiempo de espera agotado. Cerrando aplicación.");
+            loginVista.dispose();
+            System.exit(0);
+        });
+        cierreTimer.setRepeats(false); 
+        cierreTimer.start();
+    }
 }
